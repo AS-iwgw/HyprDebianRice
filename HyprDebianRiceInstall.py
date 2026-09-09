e0e81a606ad18940ff0f3bf1a8e06867a1ba5c76
AS-iwgw <326220133+AS-iwgw@users.noreply.github.com>
2026-09-09 21:58:56 +0200

    Added package install logic

--- HyprDebianRiceInstall.py
+++ HyprDebianRiceInstall.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # HyprDebianRice Install
-# This script checks if you are using an apt-based paketmanager and
+# This script checks if you are using an apt-based package manager and
 # if necessary, it will configure your sources.list. It will install
 # Hyprland and some other packages. The packages are listed in Hypr-
 # DebianRicePackages.
@@ -11,7 +11,7 @@ import shutil
 import sys
 from pathlib import Path
 
-# 1. Debian and version check
+# 1. Debian check and adding backports
 os_release = {}
 
 for line in Path("/etc/os-release").read_text().splitlines():
@@ -27,39 +27,68 @@ version = os_release.get("VERSION_ID")
 
 if version == "13":
     print("Debian 13 detected")
-    sources = """Types: deb deb-src
+    sources = """Types: deb deb-src
 URIs: https://deb.debian.org/debian
 Suites: trixie-backports
 Components: main
 Enabled: yes
 Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
-"""  
-    try:
-        subprocess.run(
-            ["sudo", "tee", "/etc/apt/sources.list.d/debian-backports.sources"],
-            input=sources,
-            text=True,
-            stdout=subprocess.DEVNULL,
-            check=True
-        )
+"""
+    try:
+        subprocess.run(
+            ["sudo", "tee", "/etc/apt/sources.list.d/debian-backports.sources"],
+            input=sources,
+            text=True,
+            stdout=subprocess.DEVNULL,
+            check=True
+        )
 
-        subprocess.run(
-            ["sudo", "apt", "update"],
-            check=True
-        )
+        subprocess.run(
+            ["sudo", "apt", "update"],
+            check=True
+        )
 
-        subprocess.run(
-            ["sudo", "apt", "install", "-t", "trixie-backports", "hyprland"],
-            check=True
-        )
-
-    except subprocess.CalledProcessError as e:
-        print(f"Fehler beim Ausführen eines Befehls: {e}")
-        sys.exit(1)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {e}")
+        sys.exit(1)
 
 else:
     print(f"Unsupported Debian version: {version}")
     sys.exit(1)
 
 
+# 2. Install packages
+# The file should contain a list of packages with the leading repository
+# For example:
+# stable;curl
+# backports;hyprland
 
+for line in Path("HyprDebianRicePackage.csv").read_text().splitlines():
+    key, value = line.split(";", 1)
+    if key == "backports":
+        try:
+            subprocess.run(
+                ["sudo", "apt", "install", "-y", "-t", "trixie-backports", value],
+                check=True
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"Error installing a backports package: {e}")
+            sys.exit(1)
+    elif key == "stable":
+        try:
+            subprocess.run(
+                ["sudo", "apt", "install", "-y", value],
+                check=True
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"Error installing a stable package: {e}")
+            sys.exit(1)
+    else:
+        try:
+            subprocess.run(
+                ["sudo", "apt", "install", "-y", value],
+                check=True
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"Error installing an unknown package: {e}")
+            sys.exit(1)
